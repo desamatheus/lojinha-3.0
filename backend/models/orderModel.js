0aa5d5b1fd02e48accf681418f2e5b4558513518
desamatheus <120420071+desamatheus@users.noreply.github.com>
2023-12-19 19:49:43 -0300

    add mais dep

---
+++ backend/models/orderModel.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderItems: [
+      {
+        slug: { type: String, required: true },
+        name: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+      },
+    ],
+    endereco: {
+      nomeCompleto: { type: String, required: true },
+      rua: { type: String, required: true },
+      cidade: { type: String, required: true },
+      cep: { type: String, required: true },
+      país: { type: String, required: true },
+    },
+    metodosPagamento: { type: String, required: true },
+    resultadoPagamento: {
+      id: String,
+      status: String,
+      update_time: String,
+      email_address: String,
+    },
+    itemsPrice: { type: Number, required: true },
+    shippingPrice: { type: Number, required: true },
+    taxPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    isPaid: { type: Boolean, default: false },
+    paidAt: { type: Date },
+    isDelivered: { type: Boolean, default: false },
+    deliveredAt: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
---
+++ frontend/src/components/atoms/Loading.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+
+export default function Loading() {
+  return <CircularProgress color="inherit" size={80} />;
+}
