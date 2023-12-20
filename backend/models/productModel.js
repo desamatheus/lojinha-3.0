9c0fe429c128a3902e8a85c263cbc0aa87a81558
desamatheus <120420071+desamatheus@users.noreply.github.com>
2023-12-20 16:27:53 -0300

    1551

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema(
   {
-    nome: { type: String, required: true, unique: true },
+    name: { type: String, required: true, unique: true },
     slug: { type: String, required: true, unique: true },
     image: { type: String, required: true },
     brand: { type: String, required: true },
@@ -20,3 +20,26 @@ const productSchema = new mongoose.Schema(
 
 const Product = mongoose.model('Product', productSchema);
 export default Product;
+
+// import mongoose from 'mongoose';
+
+// const productSchema = new mongoose.Schema(
+//   {
+//     nome: { type: String, required: true, unique: true },
+//     slug: { type: String, required: true, unique: true },
+//     image: { type: String, required: true },
+//     brand: { type: String, required: true },
+//     category: { type: String, required: true },
+//     description: { type: String, required: true },
+//     price: { type: Number, required: true },
+//     countInStock: { type: Number, required: true },
+//     rating: { type: Number, required: true },
+//     numReviews: { type: Number, required: true },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// const Product = mongoose.model('Product', productSchema);
+// export default Product;
--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,16 +1,29 @@
 import express from 'express';
 import Product from '../models/productModel.js';
 import data from '../data.js';
-import User from '../models/userModel.js';
 
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
-  await Product.deleteMany({});
+  await Product.remove({});
   const createdProducts = await Product.insertMany(data.products);
-
-  await User.deleteMany({});
-  const createdUsers = await User.insertMany(data.users);
-  res.send({ createdProducts, createdUsers });
+  res.send({ createdProducts });
 });
 export default seedRouter;
+
+// import express from 'express';
+// import Product from '../models/productModel.js';
+// import data from '../data.js';
+// import User from '../models/userModel.js';
+
+// const seedRouter = express.Router();
+
+// seedRouter.get('/', async (req, res) => {
+//   await Product.deleteMany({});
+//   const createdProducts = await Product.insertMany(data.products);
+
+//   await User.deleteMany({});
+//   const createdUsers = await User.insertMany(data.users);
+//   res.send({ createdProducts, createdUsers });
+// });
+// export default seedRouter;
