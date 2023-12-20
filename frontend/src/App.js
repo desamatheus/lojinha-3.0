2625c74b64d94277e5453dfc1f4f0faea2eff85f
desamatheus <120420071+desamatheus@users.noreply.github.com>
2023-12-20 13:28:31 -0300

    2.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,7 +38,7 @@ function App() {
           <Navbar bg='dark' variant='dark'>
             <Container>
               <LinkContainer to='/'>
-                <Navbar.Brand>amazona</Navbar.Brand>
+                <Navbar.Brand>lojinha 3.0</Navbar.Brand>
               </LinkContainer>
               <Nav className='me-auto'>
                 <Link to='/cart' className='nav-link'>
