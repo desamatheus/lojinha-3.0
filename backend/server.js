856ab82bf0406976d81da66f733ea40a35e4b975
desamatheus <120420071+desamatheus@users.noreply.github.com>
2023-12-20 15:54:10 -0300

    indo-pro-back

--- backend/server.js
+++ backend/server.js
@@ -2,38 +2,80 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import seedRouter from './routes/seedRoutes.js';
-import productRouter from './routes/productRoutes.js';
-import userRouter from './routes/userRoutes.js';
-import orderRouter from './routes/orderRoutes.js';
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('Entrou no banco');
+    console.log('connected to db');
   })
   .catch((err) => {
-    console.error('Erro ao  conectar:', err.message);
+    console.log(err.message);
   });
 
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-//configura o middleware no Express para processar dados codificados em formato URL
-
-app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
-app.use('/api/users', userRouter);
-app.use('/api/orders', orderRouter);
-
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
+// test
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+app.get('/api/products/slug/:slug', (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
+// import express from 'express';
+// import data from './data.js';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import seedRouter from './routes/seedRoutes.js';
+// import productRouter from './routes/productRoutes.js';
+// import userRouter from './routes/userRoutes.js';
+// import orderRouter from './routes/orderRoutes.js';
+
+// dotenv.config();
+
+// mongoose
+//   .connect(process.env.MONGODB_URI)
+//   .then(() => {
+//     console.log('Entrou no banco');
+//   })
+//   .catch((err) => {
+//     console.error('Erro ao  conectar:', err.message);
+//   });
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// //configura o middleware no Express para processar dados codificados em formato URL
+
+// app.use('/api/seed', seedRouter);
+// app.use('/api/products', productRouter);
+// app.use('/api/users', userRouter);
+// app.use('/api/orders', orderRouter);
+
+// app.use((err, req, res, next) => {
+//   res.status(500).send({ message: err.message });
+// });
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   console.log(`serve at http://localhost:${port}`);
+// });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,27 @@
-import data from './data';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import HomeScreen from './components/pages/Home';
-import ProductScreen from './components/pages/ProductScreen';
+import HomeScreen from './components/screens/HomeScreen';
+import ProductScreen from './components/screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Badge from 'react-bootstrap/Badge';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
-import CartScreen from './components/pages/CartScreen';
-import SigninScreen from './components/pages/SigninScreen';
-import ShippingAddressScreen from './components/pages/ShippingAddressScreen';
-import SignupScreen from './components/pages/SignupScreen';
-import PaymentMethodScreen from './components/pages/PaymentMethodScreen';
-import PlaceOrderScreen from './components/pages/PlaceOrderScreen';
-import OrderScreen from './components/pages/OrderScreen';
+import CartScreen from './components/screens/CartScreen';
+import SigninScreen from './components/screens/SigninScreen';
 
 function App() {
-  const { state, dispatch: ctxDispatch } = useContext(Store);
-  const { cart, userInfo } = state;
-
-  const signoutHandler = () => {
-    ctxDispatch({ type: 'USER_SIGNOUT' });
-    localStorage.removeItem('userInfo');
-    localStorage.removeItem('shippingAddress');
-    localStorage.removeItem('paymentMethod');
-  };
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
       <div className='d-flex flex-column site-container'>
-        <ToastContainer position='bottom-center' limit={1} />
         <header>
           <Navbar bg='dark' variant='dark'>
             <Container>
               <LinkContainer to='/'>
-                <Navbar.Brand>lojinha 3.0</Navbar.Brand>
+                <Navbar.Brand>amazona</Navbar.Brand>
               </LinkContainer>
               <Nav className='me-auto'>
                 <Link to='/cart' className='nav-link'>
@@ -49,27 +32,6 @@ function App() {
                     </Badge>
                   )}
                 </Link>
-                {userInfo ? (
-                  <NavDropdown title={userInfo.name} id='basic-nav-dropdown'>
-                    <LinkContainer to='/profile'>
-                      <NavDropdown.Item>User Profile</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to='/orderhistory'>
-                      <NavDropdown.Item>Order History</NavDropdown.Item>
-                    </LinkContainer>
-                    <NavDropdown.Divider />
-                    <Link
-                      className='dropdown-item'
-                      to='#signout'
-                      onClick={signoutHandler}>
-                      Sign Out
-                    </Link>
-                  </NavDropdown>
-                ) : (
-                  <Link className='nav-link' to='/signin'>
-                    Sign In
-                  </Link>
-                )}
               </Nav>
             </Container>
           </Navbar>
@@ -77,15 +39,8 @@ function App() {
         <main>
           <Container className='mt-3'>
             <Routes>
-              <Route path='/product/:slug' element={<ProductScreen />} />
               <Route path='/cart' element={<CartScreen />} />
-              <Route path='/signin' element={<SigninScreen />} />
-              <Route path='/signup' element={<SignupScreen />} />
-              <Route path='/placeorder' element={<PlaceOrderScreen />} />
-              <Route path='/order/:id' element={<OrderScreen />}></Route>
-              <Route path='/shipping' element={<ShippingAddressScreen />} />
-              <Route path='/payment' element={<PaymentMethodScreen />} />
-
+              <Route path='/product/:slug' element={<ProductScreen />} />
               <Route path='/' element={<HomeScreen />} />
             </Routes>
           </Container>
@@ -97,4 +52,106 @@ function App() {
     </BrowserRouter>
   );
 }
+
 export default App;
+
+// import data from './data';
+// import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import HomeScreen from './components/pages/Home';
+// import ProductScreen from './components/pages/ProductScreen';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Badge from 'react-bootstrap/Badge';
+// import Nav from 'react-bootstrap/Nav';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+// import Container from 'react-bootstrap/Container';
+// import { LinkContainer } from 'react-router-bootstrap';
+// import { useContext } from 'react';
+// import { Store } from './Store';
+// import CartScreen from './components/pages/CartScreen';
+// import SigninScreen from './components/pages/SigninScreen';
+// import ShippingAddressScreen from './components/pages/ShippingAddressScreen';
+// import SignupScreen from './components/pages/SignupScreen';
+// import PaymentMethodScreen from './components/pages/PaymentMethodScreen';
+// import PlaceOrderScreen from './components/pages/PlaceOrderScreen';
+// import OrderScreen from './components/pages/OrderScreen';
+
+// function App() {
+//   const { state, dispatch: ctxDispatch } = useContext(Store);
+//   const { cart, userInfo } = state;
+
+//   const signoutHandler = () => {
+//     ctxDispatch({ type: 'USER_SIGNOUT' });
+//     localStorage.removeItem('userInfo');
+//     localStorage.removeItem('shippingAddress');
+//     localStorage.removeItem('paymentMethod');
+//   };
+//   return (
+//     <BrowserRouter>
+//       <div className='d-flex flex-column site-container'>
+//         <ToastContainer position='bottom-center' limit={1} />
+//         <header>
+//           <Navbar bg='dark' variant='dark'>
+//             <Container>
+//               <LinkContainer to='/'>
+//                 <Navbar.Brand>lojinha 3.0</Navbar.Brand>
+//               </LinkContainer>
+//               <Nav className='me-auto'>
+//                 <Link to='/cart' className='nav-link'>
+//                   Carrinho
+//                   {cart.cartItems.length > 0 && (
+//                     <Badge pill bg='danger'>
+//                       {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+//                     </Badge>
+//                   )}
+//                 </Link>
+//                 {userInfo ? (
+//                   <NavDropdown title={userInfo.name} id='basic-nav-dropdown'>
+//                     <LinkContainer to='/profile'>
+//                       <NavDropdown.Item>User Profile</NavDropdown.Item>
+//                     </LinkContainer>
+//                     <LinkContainer to='/orderhistory'>
+//                       <NavDropdown.Item>Order History</NavDropdown.Item>
+//                     </LinkContainer>
+//                     <NavDropdown.Divider />
+//                     <Link
+//                       className='dropdown-item'
+//                       to='#signout'
+//                       onClick={signoutHandler}>
+//                       Sign Out
+//                     </Link>
+//                   </NavDropdown>
+//                 ) : (
+//                   <Link className='nav-link' to='/signin'>
+//                     Sign In
+//                   </Link>
+//                 )}
+//               </Nav>
+//             </Container>
+//           </Navbar>
+//         </header>
+//         <main>
+//           <Container className='mt-3'>
+//             <Routes>
+//               <Route path='/product/:slug' element={<ProductScreen />} />
+//               <Route path='/cart' element={<CartScreen />} />
+//               <Route path='/signin' element={<SigninScreen />} />
+//               <Route path='/signup' element={<SignupScreen />} />
+//               <Route path='/placeorder' element={<PlaceOrderScreen />} />
+//               <Route path='/order/:id' element={<OrderScreen />}></Route>
+//               <Route path='/shipping' element={<ShippingAddressScreen />} />
+//               <Route path='/payment' element={<PaymentMethodScreen />} />
+
+//               <Route path='/' element={<HomeScreen />} />
+//             </Routes>
+//           </Container>
+//         </main>
+//         <footer>
+//           <div className='text-center'>All rights reserved</div>
+//         </footer>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+// export default App;
